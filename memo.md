23fbda78740bf861611ec9521e8785c524e3dce9
YusukeTeo <siroha323@gmail.com>
2022-11-08 02:08:34 +0900

    breezeの設定修正 #1

--- memo.md
+++ memo.md
@@ -23,4 +23,4 @@ DB作ったら、.envファイルをいじる
 ```
 
 当たり前だがAuthは認証に関するファイル(app/Http/Controllers/Auth)
-
+<img width="1107" alt="image" src="https://user-images.githubusercontent.com/96870513/200371028-ab7bceda-d9ba-43e8-a63a-5b7c42384071.png">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
