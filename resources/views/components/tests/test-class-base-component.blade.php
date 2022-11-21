0abdcd33a8bc92b7ee89cb555cac9b7b07b1de63
YusukeTeo <siroha323@gmail.com>
2022-11-21 22:45:25 +0900

    クラスベースのコンポーネント

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    くらすべーすのこんぽーねんと
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+</div>
