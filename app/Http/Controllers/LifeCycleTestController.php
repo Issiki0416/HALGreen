19b56b81d09019ddd5d2d22a12ed7873d03d6ab8
YusukeTeo <siroha323@gmail.com>
2022-11-23 04:31:34 +0900

    サービスコンテナーの仕組み

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest(){
+
+    app()->bind('lifeCycleTest', function () {
+        return 'ライフサイクルテスト';
+    });
+    $test = app()->make('lifeCycleTest');
+
+    //サービスコンテナなし
+    //つまりnewがいる
+    // $message = new Message();
+    // $sample = new Sample($message);
+    // $sample->run();
+
+    //サービスコンテナあり
+    app()->bind('sample', Sample::class);
+    $sample = app()->make('sample');
+    $sample->run();
+
+    // サービスコンテナの登録されていいるものを確認する
+    dd(app(), $test);
+    }
+}
+class Sample
+{
+    public $message;
+    // 引数にクラスを入れてあげると自動でインスタンス化してくれる→DI
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send()
+    {
+        echo 'インスタンスが生成されました';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,7 @@ Route::get('/dashboard', function () {
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 
 
 require __DIR__.'/auth.php';
