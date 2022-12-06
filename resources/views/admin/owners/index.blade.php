63bf94af666285f789519a21d9b17ddaa9c4f952
Issiki0416 <siroha323@gmail.com>
2022-12-06 19:40:27 +0900

    route、viewの調整

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="md:p-6 bg-white border-b border-gray-200">
                 <section class="text-gray-600 body-font">
-                    <div class="container px-5 mx-auto">
+                    <div class="container md:px-5 mx-auto">
                         <x-flash-message status="{{ session('status') }}" />
 
                         <div class="flex justify-end mb-4">
@@ -20,20 +20,20 @@
                         <table class="table-auto w-full text-left whitespace-no-wrap">
                             <thead>
                                 <tr>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200 rounded-tl rounded-bl">オーナー名</th>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200">メールアドレス</th>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200">作成日</th>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200 rounded-tr rounded-br"></th>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200 rounded-tr rounded-br"></th>
+                                    <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200 rounded-tl rounded-bl">オーナー名</th>
+                                    <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200">メールアドレス</th>
+                                    <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200">作成日</th>
+                                    <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200 rounded-tr rounded-br"></th>
+                                    <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-yellow-200 rounded-tr rounded-br"></th>
                                 </tr>
                             </thead>
                             <tbody>
                                     @foreach ($owners as $owner)
                                 <tr>
-                                    <td class="px-4 py-3">{{ $owner->name }}</td>
-                                    <td class="px-4 py-3">{{ $owner->email }}</td>
-                                    <td class="px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
-                                    <td class="px-4 py-3">
+                                    <td class="md:px-4 py-3">{{ $owner->name }}</td>
+                                    <td class="md:px-4 py-3">{{ $owner->email }}</td>
+                                    <td class="md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
+                                    <td class="md:px-4 py-3">
                                         {{-- オーナー情報編集ボタン --}}
                                         <button onclick="location.href='{{ route('admin.owners.edit', ['owner'=> $owner->id])}} '" type="submit" class=" text-white bg-yellow-400 border-0 py-2 px-4 focus:outline-none hover:bg-yellow-500 rounded">編集</button>
                                     </td>
--- routes/admin.php
+++ routes/admin.php
@@ -23,12 +23,12 @@ use App\Http\Controllers\Admin\OwnersController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
 Route::resource('owners', OwnersController::class)
-->middleware('auth:admin');
+->middleware('auth:admin')->except(['show']);
 
 // 期限切れオーナー取得
 Route::prefix('expired-owners')-> middleware('auth:admin')->group(function(){
