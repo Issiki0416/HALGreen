8836bd3ff3b6c9f436711d8373f4b84aaf38f4e0
Issiki0416 <siroha323@gmail.com>
2022-12-11 16:39:32 +0900

    edit、updateの追加

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -88,17 +88,6 @@ class ImageController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
@@ -106,7 +95,9 @@ class ImageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        // dd(Shop::findOrFail($id));
+        return view('owner.images.edit', compact('image'));
     }
 
     /**
@@ -118,7 +109,20 @@ class ImageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => ['string', 'max:50'],
+        ]);
+
+        $image = Image::findOrFail($id);
+        $image->title = $request->title;
+        $image->save();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with([
+            'message' => '画像情報を更新しました。',
+            'status' => 'info'
+        ]);
     }
 
     /**
