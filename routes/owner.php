de16be131a79dde06bee5fffe3aa776f7ea6cd55
Issiki0416 <siroha323@gmail.com>
2023-01-03 22:39:44 +0900

    ownerのrouteを修正

--- routes/owner.php
+++ routes/owner.php
@@ -26,9 +26,9 @@ use App\Http\Controllers\Owner\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('owner.welcome');
-});
+// Route::get('/', function () {
+//     return view('owner.welcome');
+// });
 
 Route::prefix('shops')-> middleware('auth:owners')->group(function(){
     Route::get('index', [ShopController::class, 'index'])->name('shops.index');
@@ -50,10 +50,10 @@ Route::get('/dashboard', function () {
 
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //             ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
