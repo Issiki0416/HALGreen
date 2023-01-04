0d53dddaa712e9aee1c85b2a9cf09563a8c7e9d8
Issiki0416 <siroha323@gmail.com>
2023-01-04 18:04:53 +0900

    productのfactory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'information' => $this->faker->realText,
+            'price' => $this->faker->numberBetween(10, 100000), 'is_selling' => $this->faker->numberBetween(0,1),
+            'sort_order' => $this->faker->randomNumber,
+            'shop_id' => $this->faker->numberBetween(1,2),
+            'secondary_category_id' => $this->faker->numberBetween(1,6),
+            'image1' => $this->faker->numberBetween(1,6),
+            'image2' => $this->faker->numberBetween(1,6),
+            'image3' => $this->faker->numberBetween(1,6),
+            'image4' => $this->faker->numberBetween(1,6),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
 use Illuminate\Database\Seeder;
 
+use App\Models\Product;
+use App\Models\Stock;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -25,5 +26,7 @@ class DatabaseSeeder extends Seeder
             // StockSeeder::class,
             UserSeeder::class,
         ]);
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
