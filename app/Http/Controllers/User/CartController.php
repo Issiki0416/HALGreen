36ebc10e88d3958f6ec3d4ee39cfe09acb379085
Issiki0416 <siroha323@gmail.com>
2023-01-07 19:38:28 +0900

    stripe前に在庫処理を行う

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 
 class CartController extends Controller
 {
@@ -55,17 +56,33 @@ class CartController extends Controller
         $products = $user->products;//すべての商品を取得
         $lineItems = [];
         foreach($products as $product){
-            // stripeの使用に乗っ取り、一つの商品の情報
-            $lineItem = [
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
-            ];
-            array_push($lineItems, $lineItem);
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');// 現在の在庫数
+
+            if($product->pivot->quantity > $quantity){
+                return redirect()->route('user.cart.index')->with('flash_message', '在庫が足りません。');// indexメソッド内の変数もviewに渡すのでredirect
+            }else{
+                // stripeの使用に乗っ取り、一つの商品の情報
+                $lineItem = [
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);
+            }
         }
         // dd($lineItems);
+        foreach($products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1,
+            ]);
+        }
+
+        dd('test');
+
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));//stripeの秘密鍵
         $session = \Stripe\Checkout\Session::create([
             'payment_method_types' => ['card'],
