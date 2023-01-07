9fb96799bddcf527e8fb1788e14476afcf873982
Issiki0416 <siroha323@gmail.com>
2023-01-07 20:00:48 +0900

    決済後のカート削除処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -70,7 +70,7 @@ class CartController extends Controller
                         'name' => $product->name,
                         'description' => $product->information,
                     ],
-            ],
+                    ],
                 'quantity' => $product->pivot->quantity,
             ];
                 array_push($lineItems, $lineItem);
@@ -92,7 +92,7 @@ class CartController extends Controller
             'payment_method_types' => ['card'],
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'),
+            'success_url' => route('user.cart.success'),
             'cancel_url' => route('user.cart.index'),
         ]);
 
@@ -100,5 +100,11 @@ class CartController extends Controller
 
         return view('user.checkout', compact('session', 'publickey'));
 
+    }
+
+    public function success()
+    {
+        Cart::where('user_id', Auth::id())->delete();
+        return redirect()->route('user.items.index');
     }
 }
