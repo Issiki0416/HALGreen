63033ba5161e74101247ecfb9ffe64f27af3242d
Issiki0416 <siroha323@gmail.com>
2023-01-09 01:14:13 +0900

    マルチログインの修正

--- config/session.php
+++ config/session.php
@@ -130,6 +130,14 @@ return [
         'SESSION_COOKIE',
         Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
+    'cookie_owner' => env(
+        'SESSION_COOKIE_OWNER',
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session_owner'
+    ),
+    'cookie_admin' => env(
+        'SESSION_COOKIE_ADMIN',
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session_admin'
+    ),
 
     /*
     |--------------------------------------------------------------------------
