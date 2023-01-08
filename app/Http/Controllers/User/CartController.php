e9a2514509a9e8d9067bb4fdd09cd8983ce0a75e
Issiki0416 <siroha323@gmail.com>
2023-01-08 22:06:15 +0900

    cartSErviceを作成

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Cart;
 use App\Models\User;
 use App\Models\Stock;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
@@ -52,6 +53,14 @@ class CartController extends Controller
 
     public function checkout()
     {
+        ///
+        $items = Cart::where('user_id', Auth::id())->get();
+
+        $products = CartService::getItemsInCart($items);
+
+
+        ///
+
         $user = User::findOrFail(Auth::id());
         $products = $user->products;//すべての商品を取得
         $lineItems = [];
